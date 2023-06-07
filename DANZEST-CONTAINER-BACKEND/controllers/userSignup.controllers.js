87c22cf000d41d3272e9ca25777910bf1e0ac02f
dannymom3 <dannymom3@gmail.com>
2023-06-07 16:14:49 -0500

    model for signup is created and skeleton for signup controllers is done

--- DANZEST-CONTAINER-BACKEND/controllers/userSignup.controllers.js
+++ DANZEST-CONTAINER-BACKEND/controllers/userSignup.controllers.js
@@ -2,8 +2,15 @@
  * check if 
      - The admin is already exist and should be one admin account. If the admin is exist throw an error - admin is already exist otherwise admin is successfully registered .
      
-     - the regular user is exist. ifExist, throw error. Otherwise, user is successfully registered.
-     NB: hashedPassword and upload profilePicture will be done on middleware and pass into routes
+     - The regular user is exist. ifExist, throw an error message. Otherwise, user is successfully registered.
+     
+
+     NB: 
+     - hashedPassword and upload profilePicture will be done on middleware and pass into routes
+     - The admin should be unique and one account allowed to create.
+     - The regular user should be unique and can be multiple user account created.
+     
+
 */
 
 exports.signup = async (req, res, next) => {
